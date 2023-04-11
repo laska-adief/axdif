95126e2d445458683da1ab988a1bafe2a8b71663
laska-adief <laskaadief.la@gmail.com>
2023-04-11 11:25:03 +0700

    [axdif] add today-attendance component

---
+++ src/app/pages/dashboard/overview/today-attendance/today-attendance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today-attendance',
+  templateUrl: './today-attendance.component.html',
+  styleUrls: ['./today-attendance.component.scss']
+})
+export class TodayAttendanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
